8044b40a7689e35d183ba7c8e116038e90aaa184
Leticia Rodrigues dos Santos <leticia.rsantos@sptech.school>
2025-06-03 07:38:26 -0300

    Script Filmes e modelagem atualizada

---
+++ Banco de dados/Script Filmes.sql
@@ -0,0 +1,41 @@
+-- Script Filmes
+-- Projeto individual
+
+create database LeCinema;
+
+use LeCinema;
+
+create table Usuario (
+IdUsuario int primary key auto_increment,
+Nome varchar(70),
+Email varchar(70),
+Senha char(6)
+);
+
+create table Sugestao (
+IdSugestao int primary key auto_increment,
+FilmeSugerido varchar(40),
+fkUsuario int,
+constraint fkUsuarioSugestao foreign key (fkUsuario)
+	references Usuario(IdUsuario)
+);
+
+create table Jogo (
+IdJogo int primary key auto_increment,
+QtdAcertos int,
+QtdErros int
+);
+
+-- Tabela associativa
+create table Resultado (
+IdResultado int,
+fkUsuario int,
+fkJogo int,
+DataJogo date,
+constraint pkCompostaResUsuar primary key (IdResultado, fkUsuario, fkJogo),
+constraint fkUsuarioResultado foreign key (fkUsuario)
+	references Usuario(IdUsuario),
+constraint fkJogoResultado foreign key (fkJogo)
+	references Jogo(IdJogo)
+);
+
